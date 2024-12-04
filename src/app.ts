1014b86f54213acf8474853244746c0af61b26e8
shahinexy <shahinexy@gmail.com>
2024-12-04 23:21:08 +0600

    problem 01 solved

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,9 @@
-const num: string = 'shahin'
-console.log(num);
+// Problem solving 
+
+// 01. Design a TypeScript function repeatString that takes two parameters: a string and a number. The function should return a new string that repeats the input string the specified number of times.
+
+const repeatString = (str: string, num: number) =>{
+    return str.repeat(num)
+}
+
+console.log(repeatString('Hello!', 5));
