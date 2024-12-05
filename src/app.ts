9213ac0b442e4694ec11f42060f2a4fb7ecd385c
shahinexy <shahinexy@gmail.com>
2024-12-05 11:19:16 +0600

    problem 05 solved

--- src/app.ts
+++ src/app.ts
@@ -27,8 +27,27 @@ console.log(filterEvenNumbers([3,4,8,7,9,2]));
 
 // 04. Design a TypeScript function reverseArray that takes an array of any type and returns a new array with its elements reversed. Use generics to define types.
 
-const reverseArray = (arr: any[]): any[] =>{
+const reverseArray = <T>(arr: T[]): T[] =>{
     return arr.reverse();
 } 
 
-console.log(reverseArray([1,2,3,4,5]));
+console.log(reverseArray([1,2,3,4,5])); 
+
+// 05. You are given an object called “student” representing a student's information with the following properties: name (string), age (number), and grades (array of numbers). Make an interface using given properties. Write a TypeScript function called calculateAverageGrade that takes this student object as input and calculates the average grade.
+
+// Hints: Use a reduce() method to sum the grades. average = total grades/ total length
+
+interface student {
+    name: string,
+    age: number,
+    grades: number[]
+}
+
+const student1 : student = {
+    name : 'shahin',
+    age : 20,
+    grades : [95,96,90,98,99,93]
+}
+
+const averageGradeForBob = student1.grades.reduce((num, prev) => num + prev, 0)
+console.log(averageGradeForBob);
