39a1f5f9873cab1c4a9a1fc9f8fbe7ffb387cee7
shahinexy <shahinexy@gmail.com>
2024-12-05 12:58:42 +0600

    problem 08 solved

--- src/app.ts
+++ src/app.ts
@@ -98,3 +98,18 @@ const logString = (params: unknown): void =>{
 
 logString('Shahin');
 logString(234);
+
+// 08. Design a TypeScript class Car that represents a basic car with properties for brand, model, and year. Implement a method displayInfo that displays information about the car.
+
+
+class Car {
+    constructor(public brand: string, public model: string, public year: number){}
+
+    displayInfo(){
+        return `Your car model is:  ${this.year} ${this.brand} ${this.model}`;
+    }
+}
+
+const myCar = new Car('Rolls Royce', "CM20", 2024)
+
+console.log(myCar.displayInfo());
